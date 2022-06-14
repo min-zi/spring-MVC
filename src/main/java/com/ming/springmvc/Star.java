e46d4bb7d6fa41d322890c01b4fb8d0e9cd5d447
min-zi <min-zi@naver.com>
2022-06-14 22:31:56 +0900

    MVC Response와 Request 공부

---
+++ src/main/java/com/ming/springmvc/Star.java
@@ -0,0 +1,23 @@
+package com.ming.springmvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Star {
+    String name;
+    int age;
+
+    /*
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    */
+}
